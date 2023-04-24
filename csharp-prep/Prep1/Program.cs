702e044c56e92c5b9de1379a96cba0a0179cc6ed
Andrew McKenzie <heldiggris7@gmail.com>
2023-04-24 11:30:20 -0600

    prep 1 && 2

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,18 +1,16 @@
 using System;
-using static System.Console;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Write("What is your first name? ");
-        string firstName = ReadLine();
+         Console.Write("What is your first name? ");
+        string firstName =  Console.ReadLine();
 
-        Write("What is your last name? ");
-        string lastName = ReadLine();
+         Console.Write("What is your last name? ");
+        string lastName =  Console.ReadLine();
         
-        WriteLine("");
-        WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
+         Console.WriteLine("");
+         Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
 
     }
 }
