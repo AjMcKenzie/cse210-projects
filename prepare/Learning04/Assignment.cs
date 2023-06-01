917f5442d894c23333dd8c97cd8cee37044f6759
Andrew McKenzie <heldiggris7@gmail.com>
2023-05-31 18:31:31 -0600

    Assignments_Learning04

--- prepare/Learning04/Assignment.cs
+++ prepare/Learning04/Assignment.cs
@@ -1,5 +1,17 @@
 using static System.Console;
 
 public class Assignment{
+    
+    protected string _studentName;
+    private string _topic;
 
+
+    public Assignment(string name, string topic){
+        _studentName = name;
+        _topic = topic;
+    }
+
+    public  string GetSummary(){
+        return $"{_studentName} - {_topic}";
+    }
 }
--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,25 @@
 using System;
+using static System.Console;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        // Console.WriteLine("Hello Learning04 World!");
+
+        Assignment sam = new Assignment("Samuel Bennett", "Multiplication");
+        MathAssignment math1 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        WritingAssignment writing1 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+
+        WriteLine(sam.GetSummary());
+        WriteLine("");
+
+        WriteLine(math1.GetSummary());
+        WriteLine(math1.GetHomeworkList());
+        
+        WriteLine("");
+
+        WriteLine(writing1.GetSummary());
+        WriteLine(writing1.GetWritingInformation());
     }
 }
